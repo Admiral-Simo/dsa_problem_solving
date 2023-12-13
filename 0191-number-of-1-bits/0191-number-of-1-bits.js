da3a9cf8b2b432312a86bd4cada0c9019d04fad5
Simo <109309386+Admiral-Simo@users.noreply.github.com>
2023-12-13 14:44:38 +0000

    Time: 65 ms (13.37%), Space: 42.3 MB (49.81%) - LeetHub

---
+++ 0191-number-of-1-bits/0191-number-of-1-bits.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number} n - a positive integer
+ * @return {number}
+ */
+
+
+
+var hammingWeight = function(n) {
+    let res = 0;
+    for (let i = 0; i < 32; i++) {
+        if ((n >> i)&1) {
+            res += 1;
+        }
+    }
+    return res;   
+};
