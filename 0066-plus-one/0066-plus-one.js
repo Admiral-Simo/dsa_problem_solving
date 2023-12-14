f9c96d92072da0bcf8c6bdc74aebc92b540e6444
Simo <109309386+Admiral-Simo@users.noreply.github.com>
2023-12-14 17:49:42 +0000

    Time: 41 ms (96.34%), Space: 42.1 MB (23.73%) - LeetHub

--- 0066-plus-one/0066-plus-one.js
+++ 0066-plus-one/0066-plus-one.js
@@ -5,14 +5,19 @@
 var plusOne = function(digits) {
     let carry = 1;
     let result = [];
-    for(let i = digits.length - 1; i >= 0; i--) {
+
+    for (let i = digits.length - 1; i >= 0; i--) {
         let sum = digits[i] + carry;
-        result.unshift(sum % 10);
+        digits[i] = sum % 10;
         carry = Math.floor(sum / 10);
     }
-    while(carry > 0) {
-        result.unshift(carry % 10);
+
+    while (carry > 0) {
+        result.push(carry % 10);
         carry = Math.floor(carry / 10);
     }
+
+    result = result.concat(digits);
+
     return result;
 };
