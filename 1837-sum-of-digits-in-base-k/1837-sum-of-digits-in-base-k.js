74b95a089864c6db3aab07b4cc899e7e2b157242
Simo <109309386+Admiral-Simo@users.noreply.github.com>
2023-12-15 12:10:38 +0000

    Time: 52 ms (40.90%), Space: 41.6 MB (55.36%) - LeetHub

--- 1837-sum-of-digits-in-base-k/1837-sum-of-digits-in-base-k.js
+++ 1837-sum-of-digits-in-base-k/1837-sum-of-digits-in-base-k.js
@@ -1,14 +1,8 @@
 var sumBase = function(n, k) {
-    let i = 0, basek = 0;
-    while(n > 0) {
-        basek += (n % k) * (10 ** i);
-        n = Math.floor(n/k);
-        i++;
-    }
     let sum = 0;
-    while (basek > 0 ){
-        sum += basek % 10;
-        basek = Math.floor(basek / 10);
+    while(n > 0) {
+        sum += (n % k);
+        n = Math.floor(n/k);
     }
     return sum;
 };
