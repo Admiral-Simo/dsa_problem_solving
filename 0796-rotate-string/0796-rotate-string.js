e1bf4436fcdd972057a517048f7460ed9d7065b2
Simo <109309386+Admiral-Simo@users.noreply.github.com>
2023-12-15 11:36:28 +0000

    Time: 55 ms (32.63%), Space: 41.9 MB (48.68%) - LeetHub

--- 0796-rotate-string/0796-rotate-string.js
+++ 0796-rotate-string/0796-rotate-string.js
@@ -3,22 +3,10 @@
  * @param {string} goal
  * @return {boolean}
  */
-const rotate = (s) => {
-    s = s.split('');
-    const len = s.length;
-    let tmp = s[0];
-    for(let i = 0;i < s.length - 1; i++) {
-        s[i] = s[i+1];
-    }
-    s[len - 1] = tmp;
-    s = s.join('');
-    return s;
-}
+
 
 var rotateString = function(s, goal) {
-    for(let i = 0; i < s.length; i++) {
-        s = rotate(s);
-        if (s === goal) return true;
-    }
-    return false;
+    if (s.length !== goal.length) return false;
+    s += s;
+    return s.includes(goal);
 };
