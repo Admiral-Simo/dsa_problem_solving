0d12efe308f405336bf3391b3d4db2abb10f5115
Simo <109309386+Admiral-Simo@users.noreply.github.com>
2023-12-17 13:44:10 +0000

    Time: 56 ms (74.45%), Space: 46.2 MB (35.43%) - LeetHub

--- 0136-single-number/0136-single-number.js
+++ 0136-single-number/0136-single-number.js
@@ -3,12 +3,10 @@
  * @return {number}
  */
 var singleNumber = function(nums) {
-  console.log('something')
   const arr = {};
   for(let i = 0; i < nums.length; i++) {
     arr[nums[i]] = arr[nums[i]] ? arr[nums[i]] + 1 : 1;
   }
-  console.log(arr);
   for(let key in arr) {
     if (arr[key] === 1) return key;
   }
