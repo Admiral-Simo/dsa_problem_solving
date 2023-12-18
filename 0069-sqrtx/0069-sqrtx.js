b4f49fb470203404eeac908a5f0fca1b4bdd134a
Simo <109309386+Admiral-Simo@users.noreply.github.com>
2023-12-18 20:15:10 +0000

    Time: 86 ms (20.19%), Space: 43.6 MB (55.21%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.js
+++ 0069-sqrtx/0069-sqrtx.js
@@ -1,13 +1,6 @@
-/**
- * @param {number} x
- * @return {number}
- */
-
-function mySqrt(num) {
-    if (num === 0) return 0; 
-    let initial = num / 2;
-    while (Math.abs(initial * initial - num) > 0.0001) {
-        initial = 0.5 * ( initial + num/initial ); 
+const mySqrt = (num) => {
+    if (num === 0 || num === 1) return num;
+    for(let i = 1; i <= num; i++) {
+        if (i * i > num) return i - 1;
     }
-   return Math.floor(initial); 
 }
