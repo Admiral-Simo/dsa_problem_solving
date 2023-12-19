ffb5fc72a55f1f7ce5b60d7072061af63ae7c3fc
Simo <109309386+Admiral-Simo@users.noreply.github.com>
2023-12-19 22:28:37 +0000

    Time: 53 ms (66.02%), Space: 42.1 MB (21.07%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.js
+++ 0509-fibonacci-number/0509-fibonacci-number.js
@@ -2,10 +2,11 @@
  * @param {number} n
  * @return {number}
  */
-var fib = function(n, memo = {}) {
-    if (n === 0) return 0;
-    if (n <= 2) return 1;
-    if (memo[n]) return memo[n];
-    memo[n] = fib(n - 1) + fib(n - 2);
-    return memo[n];
+var fib = function(n) {
+    if (n <= 1) return n;
+    let sum = [0, 1];
+    for(let i = 2; i < n; i++) {
+        sum[i] = sum[i-1] + sum[i-2];
+    }
+    return sum[n-1] + sum[n-2];
 };
