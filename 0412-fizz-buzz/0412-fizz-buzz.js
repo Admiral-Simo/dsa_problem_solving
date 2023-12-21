beb402dfb1c8e555434d6ea9082b896c2d9b521d
Simo <109309386+Admiral-Simo@users.noreply.github.com>
2023-12-21 06:01:04 +0000

    Time: 51 ms (89.08%), Space: 44.7 MB (25.51%) - LeetHub

--- 0412-fizz-buzz/0412-fizz-buzz.js
+++ 0412-fizz-buzz/0412-fizz-buzz.js
@@ -5,15 +5,20 @@
 var fizzBuzz = function(n) {
     let result = [];
     for(let i = 1; i <= n; i++) {
-        if (i % 3 === 0 && i % 5 === 0) {
-            result.push("FizzBuzz");
-        } else if (i % 3 === 0) {
-            result.push("Fizz");
-        } else if (i % 5 === 0) {
-            result.push("Buzz");
-        } else {
-            result.push(i.toString());
+        switch(true) {
+            case i % 3 === 0 && i % 5 === 0:
+                result.push("FizzBuzz");
+                break;
+            case i % 3 === 0:
+                result.push("Fizz");
+                break;
+            case i % 5 === 0:
+                result.push("Buzz");
+                break;
+            default:
+                result.push(i.toString());     
         }
+
     }
     return result;
 };
