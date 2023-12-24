8fc9914e942e8303bfa2fe4c5346f57b20e62e8b
Simo <109309386+Admiral-Simo@users.noreply.github.com>
2023-12-24 14:11:31 +0000

    Time: 52 ms (45.82%), Space: 42 MB (18.27%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.js
+++ 0070-climbing-stairs/0070-climbing-stairs.js
@@ -3,9 +3,9 @@
  * @return {number}
  */
 
-const climbStairs = (target,n = 0, memo = {}) => {
+const climbStairs = (target,n = 0, memo = []) => {
     // goal what's the number of possibilities to reach the sum n with only positive numbers zero not included n is included
-    if (memo[n]) return memo[n];
+    if (typeof memo[n] !== 'undefined') return memo[n];
     if (n > target) return 0;
     if (n === target) return 1;
     memo[n] = climbStairs(target,n + 1, memo ) + climbStairs(target,n + 2, memo )
