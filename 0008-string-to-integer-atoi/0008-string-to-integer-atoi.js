9838c2ebf5a09b09321e614d374cca3a87ee34ea
Simo <109309386+Admiral-Simo@users.noreply.github.com>
2023-12-28 16:34:49 +0000

    Time: 60 ms (77.69%), Space: 44 MB (70.05%) - LeetHub

--- 0008-string-to-integer-atoi/0008-string-to-integer-atoi.js
+++ 0008-string-to-integer-atoi/0008-string-to-integer-atoi.js
@@ -1,32 +1,9 @@
 var myAtoi = (s) => {
-  s = s.trimStart();
-  const max32BitInt = 2 ** 31 - 1;
-  const min32BitInt = 2 ** 31 * -1;
-  const zeroCharCode = "0".charCodeAt(0);
-  const nineCharCode = "9".charCodeAt(0);
-  const firstChar = s[0];
-  let result = "0";
-  let sign = 1;
-  let idx = 0;
-
-  if (firstChar === "-" || firstChar === "+") {
-    if (firstChar === "-") sign = -1;
-    idx = 1;
-  }
-
-  for (let i = idx; i < s.length; i++) {
-    const char = s[i];
-    const code = char.charCodeAt(0);
-
-    if (code < zeroCharCode || code > nineCharCode) break;
-
-    result = result === "0" ? char : result + char;
-  }
-
-  result = +result * sign;
-
-  if (result > max32BitInt) result = max32BitInt;
-  if (result < min32BitInt) result = min32BitInt;
-
-  return result;
-};
+    const max32BitInt = 2 ** 31 - 1;
+    const min32BitInt = 2 ** 31 * -1;
+    let num = parseInt(s);
+    if (Number.isNaN(num)) num = 0;
+    if (num > max32BitInt) return max32BitInt;
+    if (num < min32BitInt) return min32BitInt;
+    return num;
+  };
