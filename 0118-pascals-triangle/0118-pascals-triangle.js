7b9ed58035ba21d516c0b06326ef6605d91113c3
Simo <109309386+Admiral-Simo@users.noreply.github.com>
2023-12-29 19:14:36 +0000

    Time: 72 ms (5.08%), Space: 42.8 MB (15.16%) - LeetHub

--- 0118-pascals-triangle/0118-pascals-triangle.js
+++ 0118-pascals-triangle/0118-pascals-triangle.js
@@ -3,16 +3,19 @@
  * @return {number[][]}
  */
 const generate = (n) => {
-    let result = [];
-    for(let i = 0; i < n; i++) {
-        let row = [];
-        for(let j = 0; j <= i; j++) {
-            const left = (result[i - 1] && result[i - 1][j - 1]) || 0;
-            const right = (result[i - 1] && result[i - 1][j]) || 0;
+    const result = [];
 
-            row.push(left + right || 1)
+    for (let i = 0; i < n; i++) {
+        const row = [];
+        let coeff = 1;
+
+        for (let j = 0; j <= i; j++) {
+            row.push(coeff);
+            coeff = Math.floor(coeff * (i - j) / (j + 1));
         }
+
         result.push(row);
-    }   
+    }
+
     return result;
-}
+};
