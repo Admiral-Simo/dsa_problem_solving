9a1050c810f8d8fe725e339f4c37e7daed3c6c4b
Simo <109309386+Admiral-Simo@users.noreply.github.com>
2023-12-30 15:43:38 +0000

    Time: 119 ms (5.20%), Space: 46.2 MB (31.09%) - LeetHub

--- 1920-build-array-from-permutation/1920-build-array-from-permutation.js
+++ 1920-build-array-from-permutation/1920-build-array-from-permutation.js
@@ -5,7 +5,7 @@
 var buildArray = function(nums) {
     let result = [];
     for(let i = 0; i < nums.length; i++) {
-        result.push(nums[nums[i]]);
+        result[i] = nums[nums[i]];
     }    
     return result;
 };
