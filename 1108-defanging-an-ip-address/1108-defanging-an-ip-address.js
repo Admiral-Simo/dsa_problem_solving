140650d5fc1837c7ebff6cf4786b80d68898d920
Simo <109309386+Admiral-Simo@users.noreply.github.com>
2023-12-30 16:29:37 +0000

    Time: 75 ms (5.65%), Space: 41.9 MB (39.67%) - LeetHub

--- 1108-defanging-an-ip-address/1108-defanging-an-ip-address.js
+++ 1108-defanging-an-ip-address/1108-defanging-an-ip-address.js
@@ -3,13 +3,5 @@
  * @return {string}
  */
 var defangIPaddr = (address) => {
-   let result = '';
-   for (let i = 0; i < address.length; i++) {
-     if (address.charAt(i) === '.') {
-        result += "[.]";
-        continue;
-     }
-     result += address.charAt(i);
-   }
-   return result; 
+    return address.replace(/\./g, "[.]");
 };
