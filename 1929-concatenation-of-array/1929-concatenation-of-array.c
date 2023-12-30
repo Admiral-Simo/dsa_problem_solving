feb524c1d77ed1ea47d95afc9084af23f64f0ec7
Simo <109309386+Admiral-Simo@users.noreply.github.com>
2023-12-30 11:39:00 +0000

    Time: 35 ms (9.79%), Space: 10.7 MB (22.15%) - LeetHub

--- 1929-concatenation-of-array/1929-concatenation-of-array.c
+++ 1929-concatenation-of-array/1929-concatenation-of-array.c
@@ -1,10 +1,22 @@
 int* getConcatenation(int* nums, int numsSize, int* returnSize) {
-    
-    int* new = malloc(sizeof(int) * numsSize * 2);
-    for(int i = 0; i < numsSize; i++) {
-        new[i] = nums[i];
-        new[i + numsSize] = nums[i];
-    }    
-    *returnSize = numsSize * 2;
-    return new;
+    // Allocate memory for the concatenated array
+    int* concatenated = malloc(sizeof(int) * numsSize * 2);
+
+    // Check for memory allocation failure
+    if (!concatenated) {
+        perror("Memory allocation failed");
+        exit(EXIT_FAILURE);
+    }
+
+    // Copy elements from nums to concatenated
+    for (int i = 0; i < numsSize; ++i) {
+        concatenated[i] = concatenated[i + numsSize] = nums[i];
+    }
+
+    // Set the returnSize value
+    if (returnSize != NULL) {
+        *returnSize = numsSize * 2;
+    }
+
+    return concatenated;
 }
