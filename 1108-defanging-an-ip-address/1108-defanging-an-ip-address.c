dfa53793f1d7fd66b87bb9ac1579b1e523531a13
Simo <109309386+Admiral-Simo@users.noreply.github.com>
2023-12-30 16:28:03 +0000

    Time: 5 ms (19.80%), Space: 6.3 MB (52.68%) - LeetHub

---
+++ 1108-defanging-an-ip-address/1108-defanging-an-ip-address.c
@@ -0,0 +1,18 @@
+char * defangIPaddr(char * address){
+    int len = strlen(address), j = 0;
+    char* newStr = malloc(sizeof(char) * (len + 7));
+    if (newStr == NULL) {
+        return NULL;
+    }
+    for(int i = 0; i < len; i++) {
+        if (address[i] == '.') {
+            newStr[j++] = '[';
+            newStr[j++] = '.';
+            newStr[j++] = ']';
+        } else {
+            newStr[j++] = address[i];
+        }
+    }
+    newStr[j] = '\0';
+    return newStr;
+}
