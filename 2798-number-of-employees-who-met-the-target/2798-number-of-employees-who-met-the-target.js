be1cc83aa7653e99543ac3b9bea90c208f4bd33d
Simo <109309386+Admiral-Simo@users.noreply.github.com>
2024-01-01 17:15:14 +0000

    Time: 105 ms (5.11%), Space: 43.7 MB (57.93%) - LeetHub

---
+++ 2798-number-of-employees-who-met-the-target/2798-number-of-employees-who-met-the-target.js
@@ -0,0 +1,12 @@
+/**
+ * @param {number[]} hours
+ * @param {number} target
+ * @return {number}
+ */
+var numberOfEmployeesWhoMetTarget = function(hours, target) {
+    let result = 0;
+    for(let i = 0; i < hours.length; i++) {
+        if (hours[i] >= target) result++;
+    }    
+    return result;
+}
