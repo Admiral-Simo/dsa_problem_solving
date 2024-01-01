a7ee3b6201b575b1b39d579876893f83b68021a3
Simo <109309386+Admiral-Simo@users.noreply.github.com>
2024-01-01 15:07:40 +0000

    Time: 60 ms (28.51%), Space: 42.1 MB (74.08%) - LeetHub

--- 2011-final-value-of-variable-after-performing-operations/2011-final-value-of-variable-after-performing-operations.js
+++ 2011-final-value-of-variable-after-performing-operations/2011-final-value-of-variable-after-performing-operations.js
@@ -3,15 +3,13 @@
  * @return {number}
  */
 var finalValueAfterOperations = function(operations) {
-    let mp  = {
-        '--X': -1,
-        'X++': 1,
-        'X--': -1,
-        '++X': 1,
-    }
     let x = 0;
     for(let i = 0; i < operations.length; i++) {
-        x += mp[operations[i]]
-    } 
+        if (operations[i] === '++X' || operations[i] === 'X++') {
+            x++;
+        } else {
+            x--;
+        }
+    }
     return x;
 };
