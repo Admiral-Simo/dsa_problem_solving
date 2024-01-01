5c5db1313736bbdef49ca631620de3735b82e532
Simo <109309386+Admiral-Simo@users.noreply.github.com>
2024-01-01 17:18:01 +0000

    Time: 0 ms (100.00%), Space: 6.7 MB (25.65%) - LeetHub

---
+++ 2798-number-of-employees-who-met-the-target/2798-number-of-employees-who-met-the-target.c
@@ -0,0 +1,7 @@
+int numberOfEmployeesWhoMetTarget(int* hours, int hoursSize, int target){
+    int result = 0;
+    for(int i = 0; i < hoursSize; i++) {
+        if (hours[i] >= target) result++;
+    }
+    return result;
+}
