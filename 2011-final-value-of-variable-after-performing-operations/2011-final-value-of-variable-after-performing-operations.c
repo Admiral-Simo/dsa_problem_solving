59fc63397db918681243993813e43ce41393d1d6
Simo <109309386+Admiral-Simo@users.noreply.github.com>
2024-01-01 15:14:56 +0000

    Time: 11 ms (23.13%), Space: 7.3 MB (53.92%) - LeetHub

--- 2011-final-value-of-variable-after-performing-operations/2011-final-value-of-variable-after-performing-operations.c
+++ 2011-final-value-of-variable-after-performing-operations/2011-final-value-of-variable-after-performing-operations.c
@@ -1,7 +1,7 @@
 int finalValueAfterOperations(char** operations, int operationsSize) {
     int x = 0;
     for(int i = 0; i < operationsSize; i++) {
-        if (strcmp(operations[i], "++X") == 0 || strcmp(operations[i], "X++") == 0) {
+        if (operations[i][0] == '+' || operations[i][2] == '+') {
             x++;
         } else {
             x--;
