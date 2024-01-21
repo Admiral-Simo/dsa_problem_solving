082741accecbfb6dea9b75936d73b67acdec549b
Simo <109309386+Admiral-Simo@users.noreply.github.com>
2024-01-21 19:00:09 +0000

    Time: 160 ms (42.71%), Space: 57.8 MB (5.04%) - LeetHub

--- 1299-replace-elements-with-greatest-element-on-right-side/1299-replace-elements-with-greatest-element-on-right-side.js
+++ 1299-replace-elements-with-greatest-element-on-right-side/1299-replace-elements-with-greatest-element-on-right-side.js
@@ -3,19 +3,13 @@
  * @return {number[]}
  */
 var replaceElements = function (arr) {
-  for (let i = 0; i < arr.length; ++i) {
-    let max = arr[i + 1];
-    for (let j = i + 1; j < arr.length; ++j) {
-      // getting the biggest number out of those on the right
-      if (max < arr[j]) {
-        max = arr[j];
-      }
-    }
-    if (i == arr.length - 1) {
-      arr[i] = -1;
-    } else {
-      arr[i] = max;
-    }
+  let max = arr[arr.length - 1];
+  arr[arr.length - 1] = -1;
+  for (let i = arr.length - 2; i >= 0; --i) {
+    let currentElement = arr[i];
+    arr[i] = max;
+    max = Math.max(max, currentElement);
   }
   return arr;
 };
+
