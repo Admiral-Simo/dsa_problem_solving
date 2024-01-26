76c0ad25d215e094dd344db61b66fc62b52d23a5
Simo <109309386+Admiral-Simo@users.noreply.github.com>
2024-01-26 15:48:25 +0000

    Time: 68 ms (24.59%), Space: 52.3 MB (5.39%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.js
+++ 0496-next-greater-element-i/0496-next-greater-element-i.js
@@ -4,23 +4,20 @@
  * @return {number[]}
  */
 var nextGreaterElement = function (nums1, nums2) {
-  const hashMap = new Map();
+  const hashMap = new Map(nums1.map((val, i) => [val, i]));
   const res = new Array(nums1.length).fill(-1);
-  for (let i = 0; i < nums1.length; ++i) {
-    hashMap.set(nums1[i], i);
-  }
+  const stack = [];
   for (let i = 0; i < nums2.length; ++i) {
-    if (hashMap.has(nums2[i])) {
-      for (let j = i + 1; j < nums2.length; ++j) {
-        if (nums2[j] > nums2[i]) {
-          res[hashMap.get(nums2[i])] = nums2[j];
-          break;
-        }
-      }
+    while (stack.length && nums2[i] > stack[stack.length - 1]) {
+      res[hashMap.get(stack.pop())] = nums2[i];
     }
+    if (hashMap.has(nums2[i])) stack.push(nums2[i]);
   }
   return res;
 };
+
+
+
 
 
 
