17e1f4e308db099757ad38a257feaf3cf76fb306
Mohamed Khalis <109309386+Admiral-Simo@users.noreply.github.com>
2024-02-17 13:16:21 +0000

    Time: 0 ms (100.00%), Space: 2.2 MB (24.43%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.rs
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.rs
@@ -1,14 +1,21 @@
 impl Solution {
     pub fn search_matrix(matrix: Vec<Vec<i32>>, target: i32) -> bool {
-        let rows = matrix.len();
-        let cols = matrix[0].len();
-        for i in 0..rows {
-            for j in 0..cols {
-                if matrix[i][j] == target {
-                    return true;
-                }
+        let rows = matrix.len() as i32;
+        let cols = matrix[0].len() as i32;
+        let mut left = 0;
+        let mut right = rows * cols - 1;
+        while left <= right {
+            let mid = left + (right - left) / 2;
+            let mid_value = matrix[(mid / cols) as usize][(mid % cols) as usize];
+            if mid_value == target {
+                return true;
+            } else if mid_value > target {
+                right = mid - 1;
+            } else {
+                left = mid + 1;
             }
         }
-        return false;
+        false
     }
 }
+
