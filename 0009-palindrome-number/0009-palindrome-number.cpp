4986cd45d32333044944df2ad645dc4b152fc3ae
Mohamed Khalis <109309386+Admiral-Simo@users.noreply.github.com>
2024-02-23 16:24:18 +0000

    Time: 3 ms (93.84%), Space: 9 MB (13.76%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.cpp
+++ 0009-palindrome-number/0009-palindrome-number.cpp
@@ -2,13 +2,12 @@ class Solution {
 public:
     bool isPalindrome(int x) {
         std::string s = std::to_string(x);
-        std::stack<char> something;
-        for(char c : s) {
-            something.push(c);
-        }
-        for(char c : s) {
-            if (something.top() != c) return false;
-            something.pop();
+        int left = 0;
+        int right = s.length() - 1;
+        while (left < right) {
+            if (s[left] != s[right]) return false;
+            left++;
+            right--;
         }
         return true;
     }
