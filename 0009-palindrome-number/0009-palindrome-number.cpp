e7a50fe5ee62264aa3024bece93607dd6d151eae
Mohamed Khalis <109309386+Admiral-Simo@users.noreply.github.com>
2024-02-23 16:28:20 +0000

    Time: 12 ms (37.54%), Space: 8.1 MB (47.20%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.cpp
+++ 0009-palindrome-number/0009-palindrome-number.cpp
@@ -1,14 +1,17 @@
 class Solution {
 public:
     bool isPalindrome(int x) {
-        std::string s = std::to_string(x);
-        int left = 0;
-        int right = s.length() - 1;
-        while (left < right) {
-            if (s[left] != s[right]) return false;
-            left++;
-            right--;
+        if (x < 0) // Negative numbers can't be palindromes
+            return false;
+        
+        int original = x;
+        long long rev = 0; // Use long long to handle potential overflow
+        
+        while (x) {
+            rev = rev * 10 + x % 10;
+            x /= 10;
         }
-        return true;
+        
+        return original == rev;
     }
 };
