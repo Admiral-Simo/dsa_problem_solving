636b0285ec4677cb3ce1eacccd386e162c72106c
Mohamed Khalis <109309386+Admiral-Simo@users.noreply.github.com>
2024-02-25 16:33:01 +0000

    Time: 13 ms (84.97%), Space: 33.9 MB (38.52%) - LeetHub

--- 0724-find-pivot-index/0724-find-pivot-index.cpp
+++ 0724-find-pivot-index/0724-find-pivot-index.cpp
@@ -1,18 +1,18 @@
 #include <vector>
 class Solution {
-  std::vector<int> prefixSum;
 
 public:
   int pivotIndex(std::vector<int> &nums) {
-    int count = 0;
+    int total = 0;
     for (int x : nums) {
-      count += x;
-      prefixSum.push_back(count);
+      total += x;
     }
+    int leftSum = 0;
     for (int i = 0; i < nums.size(); i++) {
-        int leftSum = i == 0 ? 0 : prefixSum[i - 1];
-        int rightSum = prefixSum[nums.size() - 1] - prefixSum[i];
-        if (leftSum == rightSum) return i;
+        int rightSum = total - leftSum - nums[i];
+      if (rightSum == leftSum)
+        return i;
+      leftSum += nums[i];
     }
     return -1;
   }
