16064106c114c3d773951c5446c6e7c963614388
Mohamed Khalis <109309386+Admiral-Simo@users.noreply.github.com>
2024-02-28 17:24:43 +0000

    Time: 12 ms (45.96%), Space: 16.1 MB (47.85%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
@@ -3,19 +3,48 @@ public:
   void setZeroes(std::vector<std::vector<int>> &matrix) {
     int n = matrix.size();
     int m = matrix[0].size();
-    std::vector<std::pair<int, int>> zeroPlaces;
+    bool firstColZero = false;
+    bool firstRowZero = false;
+
     for (int i = 0; i < n; i++) {
-      for (int j = 0; j < m; j++) {
-        if (matrix[i][j] == 0)
-          zeroPlaces.push_back(std::make_pair(i, j));
+      if (matrix[i][0] == 0) {
+        firstColZero = true;
+        break;
       }
     }
-    for (auto &[first, second] : zeroPlaces) {
-      for (int i = 0; i < m; i++) {
-        matrix[first][i] = 0;
+
+    for (int i = 0; i < m; i++) {
+      if (matrix[0][i] == 0) {
+        firstRowZero = true;
+        break;
       }
+    }
+
+    for (int i = 1; i < n; i++) {
+      for (int j = 1; j < m; j++) {
+        if (matrix[i][j] == 0) {
+          matrix[i][0] = 0;
+          matrix[0][j] = 0;
+        }
+      }
+    }
+
+    for (int i = 1; i < n; i++) {
+      for (int j = 1; j < m; j++) {
+        if (matrix[i][0] == 0 || matrix[0][j] == 0) {
+          matrix[i][j] = 0;
+        }
+      }
+    }
+
+    if (firstRowZero) {
+      for (int i = 0; i < m; i++) {
+        matrix[0][i] = 0;
+      }
+    }
+    if (firstColZero) {
       for (int i = 0; i < n; i++) {
-        matrix[i][second] = 0;
+        matrix[i][0] = 0;
       }
     }
   }
