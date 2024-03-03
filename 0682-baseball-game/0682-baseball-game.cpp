5d19b8396ff9461c37d8f11343ae1f19cc6c10b0
Mohamed Khalis <109309386+Admiral-Simo@users.noreply.github.com>
2024-03-03 14:21:44 +0000

    Time: 0 ms (100.00%), Space: 11.7 MB (57.60%) - LeetHub

--- 0682-baseball-game/0682-baseball-game.cpp
+++ 0682-baseball-game/0682-baseball-game.cpp
@@ -2,29 +2,33 @@ class Solution {
 public:
   int calPoints(std::vector<std::string> &operations) {
     int sum = 0;
-    std::vector<int> record;
+    std::stack<int> record;
     for (const std::string &s : operations) {
       if (s == "+") {
         if (record.size() >= 2) {
-          int points = record[record.size() - 2] + record[record.size() - 1];
+          int first = record.top();
+          record.pop();
+          int second = record.top();
+          record.push(first);
+          int points = first + second;
           sum += points;
-          record.push_back(points);
+          record.push(points);
         }
       } else if (s == "D") {
         if (!record.empty()) {
-          int points = record.back() * 2;
+          int points = record.top() * 2;
           sum += points;
-          record.push_back(points);
+          record.push(points);
         }
       } else if (s == "C") {
         if (!record.empty()) {
-          sum -= record.back();
-          record.pop_back();
+          sum -= record.top();
+          record.pop();
         }
       } else {
         int points = std::stoi(s);
         sum += points;
-        record.push_back(points);
+        record.push(points);
       }
     }
     return sum;
