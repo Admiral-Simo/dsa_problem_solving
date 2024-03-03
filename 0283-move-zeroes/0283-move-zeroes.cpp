f920054fdfd034eedae5d332997bdadff8859161
Mohamed Khalis <109309386+Admiral-Simo@users.noreply.github.com>
2024-03-03 00:21:15 +0000

    Time: 7 ms (99.02%), Space: 21.5 MB (52.37%) - LeetHub

---
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+  void moveZeroes(std::vector<int> &nums) {
+      int left = 0;
+      for (int i = 0; i < nums.size(); i++) {
+          if (nums[i]) nums[left++] = nums[i];
+      }
+      for(int i = left; i < nums.size(); i++ ) {
+          nums[i] = 0;
+      }
+  }
+};
