031382669bde05a0553b5b9d57ba0367a7b3a4bf
Mohamed Khalis <109309386+Admiral-Simo@users.noreply.github.com>
2024-03-04 12:26:14 +0000

    Time: 162 ms (76.60%), Space: 116.6 MB (78.32%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.cpp
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.cpp
@@ -1,66 +1,33 @@
-
 class Solution {
-    
 public:
-bool isPalindrome(ListNode* head) {
-    // If the list is empty or has only one node, it's a palindrome
-    if (head == nullptr || head->next == nullptr) return true;
-
-    // Find the middle of the linked list using slow and fast pointers
-    ListNode *slow = head, *fast = head;
-    std::stack<int> first_half;
-
-    while (fast != nullptr && fast->next != nullptr) {
-        first_half.push(slow->val);
-        slow = slow->next;
-        fast = fast->next->next;
-    }
-
-    // If the list has an odd number of nodes, skip the middle node
-    if (fast != nullptr) {
-        slow = slow->next;
-    }
-
-    // Compare the second half of the list with the elements in the stack
-    while (slow != nullptr) {
-        if (slow->val != first_half.top()) {
-            return false;
-        }
-        first_half.pop();
-        slow = slow->next;
-    }
-
-    return true;
-}
-
-// Helper function to print the linked list
-void printList(ListNode* head) {
+  ListNode *reverseList(ListNode *head) {
+    ListNode *prev = nullptr;
     while (head != nullptr) {
-        std::cout << head->val << " ";
-        head = head->next;
+      ListNode *nextNode = head->next;
+      head->next = prev;
+      prev = head;
+      head = nextNode;
     }
-    std::cout << std::endl;
-}
+    return prev;
+  }
 
-int main() {
-    // Example 1
-    ListNode *head1 = new ListNode(1);
-    head1->next = new ListNode(2);
-    head1->next->next = new ListNode(2);
-    head1->next->next->next = new ListNode(1);
+  bool isPalindrome(ListNode *head) {
+    if (head == nullptr || head->next == nullptr)
+      return true;
+    ListNode *slow = head, *fast = head;
+    while (fast != nullptr && fast->next != nullptr) {
+      slow = slow->next;
+      fast = fast->next->next;
+    }
+    ListNode *secondHalf = reverseList(slow);
 
-    std::cout << "Example 1: ";
-    printList(head1);
-    std::cout << "Is palindrome? " << std::boolalpha << isPalindrome(head1) << std::endl;
-
-    // Example 2
-    ListNode *head2 = new ListNode(1);
-    head2->next = new ListNode(2);
-
-    std::cout << "Example 2: ";
-    printList(head2);
-    std::cout << "Is palindrome? " << std::boolalpha << isPalindrome(head2) << std::endl;
-
-    return 0;
-}
+    while (secondHalf != nullptr) {
+      if (head->val != secondHalf->val) {
+        return false;
+      }
+      head = head->next;
+      secondHalf = secondHalf->next;
+    }
+    return true;
+  }
 };
