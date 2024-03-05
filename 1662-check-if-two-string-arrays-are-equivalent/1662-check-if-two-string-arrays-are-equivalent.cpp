6f01dca8bee463ced70e13a52dce8c8a3895fdbd
Mohamed Khalis <109309386+Admiral-Simo@users.noreply.github.com>
2024-03-05 22:15:00 +0000

    Time: 0 ms (100.00%), Space: 12.8 MB (77.12%) - LeetHub

--- 1662-check-if-two-string-arrays-are-equivalent/1662-check-if-two-string-arrays-are-equivalent.cpp
+++ 1662-check-if-two-string-arrays-are-equivalent/1662-check-if-two-string-arrays-are-equivalent.cpp
@@ -1,20 +1,25 @@
 class Solution {
 public:
-    bool arrayStringsAreEqual(std::vector<std::string>& word1, std::vector<std::string>& word2) {
-        int wordOneLength = word1.size();
-        int wordTwoLength = word2.size();
-        std::string first;
-        std::string second;
-        for (int i = 0; i < wordOneLength; i++) {
-            for (int j = 0; j < word1[i].size(); ++j) {
-                first += word1[i][j];
-            }
-        }
-        for (int i = 0; i < wordTwoLength; i++) {
-            for (int j = 0; j < word2[i].size(); ++j) {
-                second += word2[i][j];
-            }
-        }
-        return first == second;
-    }
+  bool arrayStringsAreEqual(std::vector<std::string> &word1,
+                            std::vector<std::string> &word2) {
+      int w1 = 0, w2 = 0;
+      int i = 0, j = 0;
+      while (w1 < word1.size() && w2 < word2.size()) {
+          if (word1[w1][i] != word2[w2][j]) {
+              return false;
+          }
+          i++;
+          j++;
+          if (i == word1[w1].size()) {
+              w1++;
+              i = 0;
+          }
+          if (j == word2[w2].size()) {
+              w2++;
+              j = 0;
+          }
+      }
+      if (w1 != word1.size() || w2 != word2.size()) return false;
+      return true;
+  }
 };
