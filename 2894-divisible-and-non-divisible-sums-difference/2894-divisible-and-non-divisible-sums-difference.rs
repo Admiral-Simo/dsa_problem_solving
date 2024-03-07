3d03f48289cc75f7c650c78c581ba0700d62ae0d
Mohamed Khalis <109309386+Admiral-Simo@users.noreply.github.com>
2024-03-07 09:08:11 +0000

    Time: 0 ms (100.00%), Space: 2.1 MB (18.42%) - LeetHub

---
+++ 2894-divisible-and-non-divisible-sums-difference/2894-divisible-and-non-divisible-sums-difference.rs
@@ -0,0 +1,14 @@
+impl Solution {
+    pub fn difference_of_sums(n: i32, m: i32) -> i32 {
+        let mut num1 = 0;
+        let mut num2 = 0;
+        for i in 1..=n {
+            if i % m != 0 {
+                num1 += i;
+            } else {
+                num2 += i;
+            }
+        }
+        num1 - num2
+    }
+}
