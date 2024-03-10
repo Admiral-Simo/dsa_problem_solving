22d05f79017f0f27ff63fadd9fd948d049a0fcd3
Mohamed Khalis <109309386+Admiral-Simo@users.noreply.github.com>
2024-03-10 13:55:13 +0000

    Time: 76 ms (51.55%), Space: 45.7 MB (59.48%) - LeetHub

---
+++ 0705-design-hashset/0705-design-hashset.cpp
@@ -0,0 +1,27 @@
+class MyHashSet {
+private:
+  std::unordered_map<int, bool> map;
+
+public:
+  MyHashSet() {}
+
+  void add(int key) {
+      map[key] = true;
+  }
+
+  void remove(int key) {
+    map[key] = false;
+  }
+
+  bool contains(int key) {
+      return map[key];
+  }
+};
+
+/**
+ * Your MyHashSet object will be instantiated and called as such:
+ * MyHashSet* obj = new MyHashSet();
+ * obj->add(key);
+ * obj->remove(key);
+ * bool param_3 = obj->contains(key);
+ */
