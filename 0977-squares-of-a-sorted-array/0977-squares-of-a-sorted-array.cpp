b5f521762f71b70989614593a1b5a48cfec3e8f4
Mohamed Khalis <109309386+Admiral-Simo@users.noreply.github.com>
2024-03-14 12:02:53 +0000

    Time: 21 ms (71.12%), Space: 28.4 MB (84.35%) - LeetHub

--- 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.cpp
+++ 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.cpp
@@ -1,11 +1,22 @@
 class Solution {
 public:
     std::vector<int> sortedSquares(const std::vector<int>& nums) {
-        std::vector<int> output;
-        for (int i = 0; i < nums.size(); i++) {
-            output.push_back(std::pow(nums[i], 2));
+        // nums is sorted in non decreasing order
+        int n = nums.size();
+        std::vector<int> output(n);
+        int left = 0, right = n - 1, index = right;
+        while (left <= right) {
+            int leftSquared = nums[left] * nums[left];
+            int rightSquared = nums[right] * nums[right];
+            if (leftSquared > rightSquared) {
+                output[index] = leftSquared;
+                left++;
+            } else {
+                output[index] = rightSquared;
+                right--;
+            }
+            index--;
         }
-        std::sort(output.begin(), output.end());
         return output;
     }
 };
