f39ee149a2f6bd3206d362d24c36fa84b744dbbc
Mohamed Khalis <109309386+Admiral-Simo@users.noreply.github.com>
2024-03-23 06:27:46 +0000

    Time: 15 ms (99.25%), Space: 27.7 MB (38.77%) - LeetHub

--- 2418-sort-the-people/2418-sort-the-people.cpp
+++ 2418-sort-the-people/2418-sort-the-people.cpp
@@ -2,15 +2,15 @@ class Solution {
 public:
   std::vector<std::string> sortPeople(std::vector<std::string> &names,
                                       std::vector<int> &heights) {
-      std::vector<std::pair<int, std::string>> names_height_map;
+      std::vector<std::string> ans;
+      std::priority_queue<std::pair<int, std::string>> pq;
       for (int i = 0; i < names.size(); i++) {
-          names_height_map.emplace_back(heights[i], names[i]);
+          pq.push({heights[i], names[i]});
       }
-      std::sort(names_height_map.begin(), names_height_map.end(), std::greater<std::pair<int, std::string>>());
-      std::vector<std::string> result;
-      for (const auto &pairs : names_height_map) {
-          result.push_back(pairs.second);
+      while (!pq.empty()) {
+            ans.push_back(pq.top().second);
+            pq.pop();
       }
-      return result;
+      return ans;
   }
 };
