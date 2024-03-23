7a4e5813abfa62212ea636bd71e52fa598f6bde5
Mohamed Khalis <109309386+Admiral-Simo@users.noreply.github.com>
2024-03-23 07:13:51 +0000

    Time: 7 ms (71.65%), Space: 19.7 MB (51.07%) - LeetHub

--- 0112-path-sum/0112-path-sum.cpp
+++ 0112-path-sum/0112-path-sum.cpp
@@ -1,12 +1,15 @@
 class Solution {
 public:
-  bool hasPathSum(TreeNode *root, int targetSum) {
-    // base case is current > targetSum return;
-      if (root == nullptr) return false;
-      if (root->left == nullptr && root->right == nullptr) {
-          return root->val == targetSum;
-      }
-      int newTarget = targetSum - root->val;
-      return hasPathSum(root->right, newTarget) || hasPathSum(root->left, newTarget);
-  }
+    bool hasPathSum(TreeNode* root, int targetSum) {
+        if (root == nullptr) return false;
+        
+        // Check if current node is a leaf node
+        if (root->left == nullptr && root->right == nullptr) 
+            return targetSum == root->val; 
+        
+        int newTarget = targetSum - root->val; // Update target sum for the next recursion
+        
+        // Recurse on left and right subtrees
+        return hasPathSum(root->left, newTarget) || hasPathSum(root->right, newTarget);
+    }
 };
