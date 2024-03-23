170e65437f6286ef2578cfebd706a122e68d72c4
Mohamed Khalis <109309386+Admiral-Simo@users.noreply.github.com>
2024-03-23 15:37:47 +0000

    Time: 0 ms (100.00%), Space: 13.8 MB (57.10%) - LeetHub

--- 1572-matrix-diagonal-sum/1572-matrix-diagonal-sum.cpp
+++ 1572-matrix-diagonal-sum/1572-matrix-diagonal-sum.cpp
@@ -4,17 +4,16 @@ public:
         int sum = 0;
         int n = mat.size();
         int m = mat[0].size();
-        for (int i = 0; i < n; i++) {
-            for(int j = 0; j < m; j++) {
-                // what is the second condition should be
-                // i = 0 j = 2
-                // i = 1 j = 1
-                // i = 2 j = 0
-                if (i == j) sum += mat[i][j];
-            }
+
+        int i = 0, j = 0;
+
+        while (i < mat.size() && j < mat.size()) {
+            if (i == j) sum += mat[i][j];
+            i++;
+            j++;
         }
 
-        int i = 0, j = mat.size() - 1;
+        i = 0, j = mat.size() - 1;
 
         while (i < mat.size() && j > -1) {
             if (i != j) sum += mat[i][j];
