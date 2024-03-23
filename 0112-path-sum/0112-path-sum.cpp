5d2447ea533010c2950de01822a88494daafdd50
Mohamed Khalis <109309386+Admiral-Simo@users.noreply.github.com>
2024-03-23 07:10:23 +0000

    Time: 13 ms (12.11%), Space: 19.8 MB (11.44%) - LeetHub

--- 0112-path-sum/0112-path-sum.cpp
+++ 0112-path-sum/0112-path-sum.cpp
@@ -1,13 +1,12 @@
 class Solution {
 public:
-  bool helper(TreeNode *root, int targetSum, int current) {
-      if (root == nullptr) return false;
-      current += root->val;
-      if (root->left == nullptr && root->right == nullptr) return current == targetSum;
-      return helper(root->left, targetSum, current) || helper(root->right, targetSum, current);
-  }
   bool hasPathSum(TreeNode *root, int targetSum) {
     // base case is current > targetSum return;
-      return helper(root, targetSum, 0);
+      if (root == nullptr) return false;
+      if (root->left == nullptr && root->right == nullptr) {
+          return root->val == targetSum;
+      }
+      int newTarget = targetSum - root->val;
+      return hasPathSum(root->right, newTarget) || hasPathSum(root->left, newTarget);
   }
 };
