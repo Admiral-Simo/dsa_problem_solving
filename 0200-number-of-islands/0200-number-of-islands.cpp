be303cc0e97c2f4753b7eab7e05944bb9bd75664
Mohamed Khalis <109309386+Admiral-Simo@users.noreply.github.com>
2024-03-30 22:24:28 +0000

    Time: 30 ms (49.25%), Space: 23.1 MB (21.89%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.cpp
+++ 0200-number-of-islands/0200-number-of-islands.cpp
@@ -1,17 +1,28 @@
 class Solution {
 private:
-  void dfs(vector<vector<char>> &grid, int r,
-           int c) {
-    if (c >= grid[0].size() || r >= grid.size() || min(c, r) < 0 ||
-        grid[r][c] == '0')
-      return;
-
+  void bfs(vector<vector<char>> &grid, int r, int c) {
+    int ROWS = grid.size(), COLS = grid[0].size();
+    queue<pair<int, int>> q;
+    q.push({r, c});
     grid[r][c] = '0';
 
-    dfs(grid, r + 1, c);
-    dfs(grid, r - 1, c);
-    dfs(grid, r, c + 1);
-    dfs(grid, r, c - 1);
+    vector<pair<int, int>> directions = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
+
+    while (!q.empty()) {
+      auto [r, c] = q.front();
+      q.pop();
+
+      for (auto &dir : directions) {
+        int new_r = r + dir.first;
+        int new_c = c + dir.second;
+
+        if (new_r >= 0 && new_r < ROWS && new_c >= 0 && new_c < COLS &&
+            grid[new_r][new_c] == '1') {
+          q.push({new_r, new_c});
+          grid[new_r][new_c] = '0';
+        }
+      }
+    }
   }
 
 public:
@@ -26,7 +37,7 @@ public:
       for (int j = 0; j < COLS; j++) {
         if (grid[i][j] == '1') {
           // if cell equals to one execute a dfs otherwise
-          dfs(grid, i, j);
+          bfs(grid, i, j);
           ++island_count;
         }
       }
