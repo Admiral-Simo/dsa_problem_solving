239ea448085b0863f8c40c5fd7439820236e0348
Mohamed Khalis <109309386+Admiral-Simo@users.noreply.github.com>
2024-04-01 02:03:42 +0000

    Time: 3 ms (84.74%), Space: 15.7 MB (94.53%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.cpp
+++ 0994-rotting-oranges/0994-rotting-oranges.cpp
@@ -1,52 +1,49 @@
 class Solution {
 public:
   int orangesRotting(vector<vector<int>> &grid) {
-    int rows = grid.size();
-    int cols = grid[0].size();
-    int freshCount = 0;
-    queue<pair<int, int>> rotten;
-    vector<pair<int, int>> directions = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
+      int rows = grid.size(), cols = grid[0].size();
+      int freshCount = 0;
+      queue<pair<int, int>> rotten;
 
-    // Initialize the queue with the positions of rotten oranges and count fresh
-    // oranges
-    for (int i = 0; i < rows; ++i) {
-      for (int j = 0; j < cols; ++j) {
-        if (grid[i][j] == 2)
-          rotten.push({i, j});
-        else if (grid[i][j] == 1)
-          ++freshCount;
-      }
-    }
-
-    int minutes = 0;
-    while (!rotten.empty()) {
-      // Check if all oranges are rotten
-      if (freshCount == 0)
-        return minutes;
-
-      // Process rotten oranges in the current minute
-      int size = rotten.size();
-      for (int i = 0; i < size; ++i) {
-        int x = rotten.front().first;
-        int y = rotten.front().second;
-        rotten.pop();
-
-        for (const auto &dir : directions) {
-          int nx = x + dir.first;
-          int ny = y + dir.second;
-          if (nx >= 0 && nx < rows && ny >= 0 && ny < cols &&
-              grid[nx][ny] == 1) {
-            grid[nx][ny] = 2;
-            --freshCount;
-            rotten.push({nx, ny});
+      for (int i = 0; i < rows; i++) {
+          for (int j = 0; j < cols; j++) {
+              if (grid[i][j] == 2) rotten.push({i, j});
+              else if (grid[i][j] == 1) freshCount++;
           }
-        }
       }
 
-      // Increment minutes
-      ++minutes;
-    }
 
-    return freshCount == 0 ? minutes : -1;
+      int minute_count = 0;
+
+      vector<pair<int, int>> directions = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
+
+      while (!rotten.empty()) {
+          if (freshCount == 0) return minute_count;
+
+          int size = rotten.size();
+          for (int i = 0; i < size; i++) {
+              int r = rotten.front().first;
+              int c = rotten.front().second;
+              rotten.pop();
+
+              for (const auto &dir : directions) {
+                      int new_r = r + dir.first;
+                      int new_c = c + dir.second;
+
+                      if (new_r >= 0 && new_c >= 0 && new_r < rows && new_c < cols && grid[new_r][new_c] == 1) {
+                          grid[new_r][new_c] = 2;
+                          rotten.push({new_r, new_c});
+                          freshCount--;
+                      }
+              }
+
+          }
+
+
+          minute_count++;
+      }
+
+      return freshCount > 0 ? -1 : minute_count;
+
   }
 };
