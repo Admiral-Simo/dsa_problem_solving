959fbb9f4afccacaedfb8603e7dd0eccf52bb21b
Mohamed Khalis <109309386+Admiral-Simo@users.noreply.github.com>
2024-04-02 19:58:46 +0000

    Time: 6 ms (28.70%), Space: 9.7 MB (28.14%) - LeetHub

--- 0535-encode-and-decode-tinyurl/0535-encode-and-decode-tinyurl.cpp
+++ 0535-encode-and-decode-tinyurl/0535-encode-and-decode-tinyurl.cpp
@@ -1,14 +1,14 @@
 class Solution {
     unordered_map<string, string> data;
-    int i;
+    const string chars = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+    const int chars_length = 62;
 public:
-    Solution() {
-        i = 0;
-    }
     string encode(string longUrl) {
         string output = "http://tinyurl.com/";
+        for (int i = 0; i < 6; i++) {
+            output.push_back(chars[rand() % chars_length]);
+        }
         // add the first 6 hashed characters from the longUrl
-        output += to_string(i++);
         data[output] = longUrl;
         return output;
     }
