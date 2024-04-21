64978fc9352a7cbc11a1403daf4599b5831e9e79
Mohamed Khalis <109309386+Admiral-Simo@users.noreply.github.com>
2024-04-21 02:33:28 +0100

    Time: 79 ms (91.15%), Space: 7.6 MB (93.77%) - LeetHub

--- 0219-contains-duplicate-ii/0219-contains-duplicate-ii.go
+++ 0219-contains-duplicate-ii/0219-contains-duplicate-ii.go
@@ -1,10 +1,34 @@
+type HashSet map[int]struct{}
+
+func NewHashSet() HashSet {
+	return make(HashSet)
+}
+
+func (set HashSet) Add(value int) {
+	set[value] = struct{}{}
+}
+
+func (set HashSet) Has(value int) bool {
+	_, ok := set[value]
+	return ok
+}
+
+func (set HashSet) Remove(value int) {
+	delete(set, value)
+}
+
 func containsNearbyDuplicate(nums []int, k int) bool {
-	hashMap := make(map[int]int)
-	for i, num := range nums {
-		if index, ok := hashMap[num]; ok && i-index <= k {
+	hashSet := NewHashSet()
+	n := len(nums)
+	for i := 0; i < n; i++ {
+		currentNum := nums[i]
+		if i > k {
+			hashSet.Remove(nums[i-k-1])
+		}
+		if hashSet.Has(currentNum) {
 			return true
 		}
-		hashMap[num] = i
+		hashSet.Add(currentNum)
 	}
 	return false
 }
