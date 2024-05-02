1226aaa3298946b1ecf0ecfd4cc11a464cbb8aeb
Mohamed Khalis <109309386+Admiral-Simo@users.noreply.github.com>
2024-05-03 00:02:36 +0100

    Time: 0 ms (100.00%), Space: 2.2 MB (28.08%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.go
+++ 0509-fibonacci-number/0509-fibonacci-number.go
@@ -1,4 +1,4 @@
-func fib(n int) int {
+func fibs(n int) int {
     if n <= 1 {
         return n
     }
@@ -7,4 +7,37 @@ func fib(n int) int {
         a, b = b, a+b
     }
     return b
+}
+
+// Matrix represents a 2x2 matrix
+type Matrix [2][2]int
+
+// Multiply multiplies two matrices
+func Multiply(a, b Matrix) Matrix {
+	return Matrix{
+		{a[0][0]*b[0][0] + a[0][1]*b[1][0], a[0][0]*b[0][1] + a[0][1]*b[1][1]},
+		{a[1][0]*b[0][0] + a[1][1]*b[1][0], a[1][0]*b[0][1] + a[1][1]*b[1][1]},
+	}
+}
+
+// Power computes the power of a matrix
+func Power(a Matrix, n int) Matrix {
+	if n == 1 {
+		return a
+	}
+	if n%2 == 0 {
+		half := Power(a, n/2)
+		return Multiply(half, half)
+	}
+	return Multiply(a, Power(a, n-1))
+}
+
+// FibonacciLogN calculates the nth Fibonacci number using logarithmic time complexity
+func fib(n int) int {
+	if n <= 1 {
+		return n
+	}
+	base := Matrix{{1, 1}, {1, 0}}
+	result := Power(base, n-1)
+	return result[0][0]
 }
