38abac63ecd5368e778ca5feace9155e3bd6fd50
Mohamed Khalis <109309386+Admiral-Simo@users.noreply.github.com>
2024-05-25 15:01:36 +0100

    Time: 0 ms (100.00%), Space: 2.3 MB (91.20%) - LeetHub

--- 3110-score-of-a-string/3110-score-of-a-string.go
+++ 3110-score-of-a-string/3110-score-of-a-string.go
@@ -1,9 +1,17 @@
+
 func scoreOfString(s string) int {
 	sum := 0
 	for i := 0; i < len(s)-1; i++ {
-		max := int(math.Max(float64(s[i]), float64(s[i+1])))
-		min := int(math.Min(float64(s[i]), float64(s[i+1])))
-		sum += max - min
+		currCharCode := int(s[i])
+		nextCharCode := int(s[i+1])
+		sum += abs(currCharCode - nextCharCode)
 	}
 	return sum
+}
+
+func abs(a int) int {
+	if a < 0 {
+		return -a
+	}
+	return a
 }
