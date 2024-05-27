471184e78f2b92db5b97d2e4f4a3a00cad5ce198
Mohamed Khalis <109309386+Admiral-Simo@users.noreply.github.com>
2024-05-27 20:28:32 +0100

    Time: 5 ms (69.15%), Space: 4.3 MB (34.63%) - LeetHub

---
+++ 0412-fizz-buzz/0412-fizz-buzz.go
@@ -0,0 +1,16 @@
+func fizzBuzz(n int) []string {
+	result := []string{}
+	for i := 1; i <= n; i++ {
+		switch {
+		case i%3 == 0 && i%5 == 0:
+			result = append(result, "FizzBuzz")
+		case i%3 == 0:
+			result = append(result, "Fizz")
+		case i%5 == 0:
+			result = append(result, "Buzz")
+		default:
+			result = append(result, fmt.Sprintf("%d", i))
+		}
+	}
+	return result
+}
