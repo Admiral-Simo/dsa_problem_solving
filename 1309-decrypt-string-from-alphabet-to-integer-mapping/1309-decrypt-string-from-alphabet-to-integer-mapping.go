f7d4c7c2e1bec6219901adadcfdd0e3555b94b89
Mohamed Khalis <109309386+Admiral-Simo@users.noreply.github.com>
2024-06-05 01:30:43 +0100

    Time: 1 ms (72.73%), Space: 2.8 MB (36.36%) - LeetHub

--- 1309-decrypt-string-from-alphabet-to-integer-mapping/1309-decrypt-string-from-alphabet-to-integer-mapping.go
+++ 1309-decrypt-string-from-alphabet-to-integer-mapping/1309-decrypt-string-from-alphabet-to-integer-mapping.go
@@ -1,33 +1,14 @@
-const alphabets = "abcdefghijklmnopqrstuvwxyz"
-
-func reverseString(s *string) {
-	runes := []rune(*s)
-	for i, j := 0, len(runes)-1; i < j; i, j = i+1, j-1 {
-		runes[i], runes[j] = runes[j], runes[i]
-	}
-	*s = string(runes)
-}
-
 func freqAlphabets(s string) string {
-	var result string
-
-	for i := len(s) - 1; i >= 0; {
-		character := 0
-		if s[i] == '#' {
-			i--
-			character += int(s[i]) - '0'
-			i--
-			character += (int(s[i]) - '0') * 10
-			i--
+	var res string
+	for i := 0; i < len(s); i++ {
+		if i+2 < len(s) && s[i+2] == '#' {
+			n, _ := strconv.Atoi(s[i : i+2])
+			res += fmt.Sprintf("%c", 'a'+n-1)
+			i += 2
 		} else {
-			character += int(s[i]) - '0'
-			i--
+			n, _ := strconv.Atoi(string(s[i]))
+			res += fmt.Sprintf("%c", 'a'+n-1)
 		}
-
-		result += fmt.Sprintf("%c", character+'a'-1)
 	}
-
-	reverseString(&result)
-
-	return result
+	return res
 }
