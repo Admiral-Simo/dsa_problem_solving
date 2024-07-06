60b119380006a171876c3f5b7b8cb1ac8aec9b75
Mohamed Khalis <109309386+Admiral-Simo@users.noreply.github.com>
2024-07-06 13:23:17 +0100

    Time: 31 ms (43.16%), Space: 6.8 MB (76.24%) - LeetHub

--- 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.go
+++ 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.go
@@ -6,34 +6,21 @@
  * }
  */
 func getIntersectionNode(headA, headB *ListNode) *ListNode {
-	aLength := getLength(headA)
-	bLength := getLength(headB)
-	difference := int(math.Abs(float64(aLength) - float64(bLength)))
-	if aLength > bLength {
-		for i := 0; i < difference; i++ {
-			headA = headA.Next
+	a, b := headA, headB
+
+	for a != b {
+		if a != nil {
+			a = a.Next
+		} else {
+			a = headB
 		}
-	} else {
-		for i := 0; i < difference; i++ {
-			headB = headB.Next
+
+		if b != nil {
+			b = b.Next
+		} else {
+			b = headA
 		}
 	}
 
-	for headA != nil {
-		if headA == headB {
-			return headA
-		}
-		headA = headA.Next
-		headB = headB.Next
-	}
-	return nil
-}
-
-func getLength(head *ListNode) int {
-	count := 0
-	for head != nil {
-		count++
-		head = head.Next
-	}
-	return count
+	return a
 }
