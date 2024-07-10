2e5f50f098f380f269cb394764ce2fbdb1c8a723
Mohamed Khalis <109309386+Admiral-Simo@users.noreply.github.com>
2024-07-10 13:15:45 +0100

    Time: 1 ms (80.00%), Space: 2.1 MB (58.75%) - LeetHub

---
+++ 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.rs
@@ -0,0 +1,9 @@
+use std::collections::HashSet;
+
+impl Solution {
+    pub fn intersection(nums1: Vec<i32>, nums2: Vec<i32>) -> Vec<i32> {
+        let set1: HashSet<i32> = nums1.into_iter().collect();
+        let set2: HashSet<i32> = nums2.into_iter().collect();
+        set1.intersection(&set2).cloned().collect()
+    }
+}
