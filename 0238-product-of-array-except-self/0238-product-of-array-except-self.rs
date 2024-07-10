4747a85e29f72879253b8596ef28ba620b5afec2
Mohamed Khalis <109309386+Admiral-Simo@users.noreply.github.com>
2024-07-10 18:43:51 +0100

    Time: 13 ms (60.98%), Space: 3.7 MB (10.47%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.rs
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.rs
@@ -1,16 +1,36 @@
 impl Solution {
     pub fn product_except_self(nums: Vec<i32>) -> Vec<i32> {
-        let mut result = vec![1; nums.len()];
-        let mut prefix = 1;
-        for i in 0..nums.len() {
-            result[i] *= prefix;
-            prefix *= nums[i];
-        }
-        let mut postfix = 1;
-        for i in (0..nums.len()).rev() {
-            result[i] *= postfix;
-            postfix *= nums[i];
-        }
-        result
+        let n = nums.len();
+
+        let prefix_products: Vec<i32> = nums
+            .iter()
+            .scan(1, |product, &x| {
+                *product *= x;
+                Some(*product)
+            })
+            .collect();
+
+        let suffix_products: Vec<i32> = nums
+            .iter()
+            .rev()
+            .scan(1, |product, &x| {
+                *product *= x;
+                Some(*product)
+            })
+            .collect();
+
+        let suffix_products: Vec<i32> = suffix_products.into_iter().rev().collect();
+
+        (0..n)
+            .map(|i| {
+                let prefix = if i == 0 { 1 } else { prefix_products[i - 1] };
+                let suffix = if i == n - 1 {
+                    1
+                } else {
+                    suffix_products[i + 1]
+                };
+                prefix * suffix
+            })
+            .collect()
     }
 }
