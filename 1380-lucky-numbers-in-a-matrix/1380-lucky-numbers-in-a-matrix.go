245b88d5f204e0faadd55c8d163d0e6df3726e42
Mohamed Khalis <109309386+Admiral-Simo@users.noreply.github.com>
2024-07-19 17:54:59 +0100

    Time: 6 ms (100.00%), Space: 5.4 MB (66.67%) - LeetHub

--- 1380-lucky-numbers-in-a-matrix/1380-lucky-numbers-in-a-matrix.go
+++ 1380-lucky-numbers-in-a-matrix/1380-lucky-numbers-in-a-matrix.go
@@ -1,26 +1,31 @@
 func luckyNumbers(matrix [][]int) []int {
 	var result []int
 	for i := 0; i < len(matrix); i++ {
-		minVal := matrix[i][0]
-		minIdx := 0
-		for j := 1; j < len(matrix[i]); j++ {
-			if matrix[i][j] < minVal {
-				minVal = matrix[i][j]
-				minIdx = j
-			}
-		}
-
-		isMax := true
-		for k := 0; k < len(matrix); k++ {
-			if matrix[k][minIdx] > minVal {
-				isMax = false
-				break
-			}
-		}
-
-		if isMax {
+		minVal, minIdx := findMinInRow(matrix[i])
+		if isMaxInColumn(matrix, minVal, minIdx) {
 			result = append(result, minVal)
 		}
 	}
 	return result
+}
+
+func findMinInRow(row []int) (int, int) {
+	minIdx := 0
+	minVal := row[0]
+	for i := 1; i < len(row); i++ {
+		if row[i] < minVal {
+			minIdx = i
+			minVal = row[i]
+		}
+	}
+	return minVal, minIdx
+}
+
+func isMaxInColumn(matrix [][]int, value int, colIdx int) bool {
+	for k := 0; k < len(matrix); k++ {
+		if matrix[k][colIdx] > value {
+			return false
+		}
+	}
+	return true
 }
