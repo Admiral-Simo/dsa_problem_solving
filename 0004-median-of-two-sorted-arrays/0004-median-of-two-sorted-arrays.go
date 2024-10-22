66b63488b2644a249f7e0133946949c09b81d5cd
Mohamed Khalis <109309386+Admiral-Simo@users.noreply.github.com>
2024-10-22 01:27:31 +0100

    Time: 0 ms (100.00%), Space: 6.5 MB (10.37%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.go
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.go
@@ -1,32 +1,60 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
 func findMedianSortedArrays(nums1 []int, nums2 []int) float64 {
-	combinedArr := []int{}
-	i, j := 0, 0
+	// Ensure nums1 is the smaller array for efficiency
+	if len(nums1) > len(nums2) {
+		return findMedianSortedArrays(nums2, nums1)
+	}
 
-	N := len(nums1)
-	M := len(nums2)
+	N, M := len(nums1), len(nums2)
+	halfLen := (N + M + 1) / 2
 
-	for i < N && j < M {
-		if nums1[i] <= nums2[j] {
-			combinedArr = append(combinedArr, nums1[i])
-			i++
+	left, right := 0, N
+	for left <= right {
+		partitionX := (left + right) / 2
+		partitionY := halfLen - partitionX
+
+		// Handle out-of-bounds by using extreme values for empty partitions
+		maxLeftX := math.Inf(-1)
+		if partitionX > 0 {
+			maxLeftX = float64(nums1[partitionX-1])
+		}
+		minRightX := math.Inf(1)
+		if partitionX < N {
+			minRightX = float64(nums1[partitionX])
+		}
+
+		maxLeftY := math.Inf(-1)
+		if partitionY > 0 {
+			maxLeftY = float64(nums2[partitionY-1])
+		}
+		minRightY := math.Inf(1)
+		if partitionY < M {
+			minRightY = float64(nums2[partitionY])
+		}
+
+		// Check if we have found the correct partition
+		if maxLeftX <= minRightY && maxLeftY <= minRightX {
+			// If total number of elements is odd, return max of left sides
+			if (N+M)%2 == 1 {
+				return math.Max(maxLeftX, maxLeftY)
+			}
+			// If total number of elements is even, return average of the middle values
+			return (math.Max(maxLeftX, maxLeftY) + math.Min(minRightX, minRightY)) / 2
+		} else if maxLeftX > minRightY {
+			// Too far right in nums1, move left
+			right = partitionX - 1
 		} else {
-			combinedArr = append(combinedArr, nums2[j])
-			j++
+			// Too far left in nums1, move right
+			left = partitionX + 1
 		}
 	}
 
-	for ; i < N; i++ {
-		combinedArr = append(combinedArr, nums1[i])
-	}
-
-	for ; j < M; j++ {
-		combinedArr = append(combinedArr, nums2[j])
-	}
-
-	totalLength := N + M
-	if totalLength%2 == 1 {
-		return float64(combinedArr[totalLength/2])
-	}
-
-	return (float64(combinedArr[totalLength/2]) + float64(combinedArr[totalLength/2-1])) / 2
+	return 0.0 // This will never be reached if input is valid
 }
+
